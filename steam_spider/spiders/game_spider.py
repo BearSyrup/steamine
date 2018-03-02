72f6bfe2e42ba4bacca553d37504870286045d5a
Michael Douglas <michaelc.araujo@gmail.com>
2018-03-01 23:28:38 -0300

    Delete the comment

--- steam_spider/spiders/game_spider.py
+++ steam_spider/spiders/game_spider.py
@@ -5,26 +5,7 @@ class Games(scrapy.Item):
     name = scrapy.Field()
     description = scrapy.Field()
     price = scrapy.Field()
-
-
-#class SteamMinePrices(scrapy.Spider):
-#    name = 'steam_spider'
-#    start_urls = ['https://store.steampowered.com/games'] 
-#
-#    def parse(self, response):
-#        pages = response.xpath("//div[@id='genre_flyout']/div/a/@href")
-#        for page in pages:
-#            link = page.extract()
-#            self.log('going to page %s' %link) 
-#            scrapy.http.Request(link, self.handle_games)
-#            self.log('hello there')
-#
-#
-#    def handle_games(self, response):
-#        games = response.xpath("//div[@id='tab_NewReleases_content']//a//div[@class='discount_prices']")
-#        self.log('hello bananas')
-#
-#        
+      
 class SteamSpider(scrapy.Spider):
     name = 'steam_spider'
 
@@ -39,7 +20,6 @@ class SteamSpider(scrapy.Spider):
     def get_games(self, response):
         games = response.xpath("//div[@id='NewReleasesRows']//a")
         for game in games:
-            #self.log('game%s'%game.extract())
             yield {
                     'price': game.xpath("div[@class='discount_block tab_item_discount no_discount']/div[@class='discount_prices']//text()").extract(),
                     }  
